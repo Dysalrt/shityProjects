e5fc7abe56025d0ae19faba65b735a2587027223
Dysalrt <paradoxumsum@gmail.com>
2026-07-04 00:24:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let p0colorInput = document.getElementById("p0-color-input");
 let p0hexCodeOutput = document.getElementById("p0-hex-code");
 let p0rgbCodeOutput = document.getElementById("p0-rgb-code");
 
-document.getElementByID("p0-submit").onclick = () => {
+document.getElementById("p0-submit").onclick = () => {
   console.log(p0colorInput.value);
 }
